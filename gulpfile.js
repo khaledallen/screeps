c26d66543c559bde28f178e61ef8776aca5101f2
Khaled Allen <khaled.allen@gmail.com>
2019-11-09 13:37:00 -0600

    added creds to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const { watch } = require('gulp');
 var screeps = require('gulp-screeps');
+const credentials = require('credentials.js');
 
 gulp.task('screeps', function() {
   gulp.src('*.js')
-    .pipe(screeps(options));
+    .pipe(screeps(credentials));
 });
 
 
