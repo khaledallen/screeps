6cb9065eb1779db1bf3690e617d2a67e40f9215b
Khaled Allen <khaled.allen@gmail.com>
2019-11-09 11:39:45 -0600

    updated .gitignore to ignore the configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 const { watch } = require('gulp');
+var screeps = require('gulp-screeps');
+
+gulp.task('screeps', function() {
+  gulp.src('*.js')
+    .pipe(screeps(options));
+});
+
 
 function defaultTask(cb) {
     console.log('change detected');
